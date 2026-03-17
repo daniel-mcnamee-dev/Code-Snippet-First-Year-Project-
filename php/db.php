953d5700549b6a35878e29575ea2938a0f60b40c
Daniel McNamee <48935313+daniel-mcnamee-dev@users.noreply.github.com>
2026-03-17 20:57:38 +0000

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,25 +1,26 @@
 <?php
+
+$host = getenv("DB_HOST");
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASS");
+$port = getenv("DB_PORT");
+
 try {
-
-    $host = getenv("DB_HOST");
-    $dbname = getenv("DB_NAME");
-    $username = getenv("DB_USER");
-    $password = getenv("DB_PASS");
-    $port = getenv("DB_PORT");
-
     $pdo = new PDO(
         "pgsql:host=$host;port=$port;dbname=$dbname",
-        $username,
+        $user,
         $password
     );
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
 
-    error_log("Database Connection Error: " . $e->getMessage());
-    die("Database connection failed.");
-
+    header('Content-Type: application/json');
+    echo json_encode([
+        "success" => false,
+        "message" => "Database connection failed"
+    ]);
+    exit;
 }
-?>
